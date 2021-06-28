f16c60a9a56a1ec014feeaf68b9ee9cb50809cfc
LorriAnn <LorriAnn7@att.net>
2021-06-27 22:29:24 -0400

    adds clickable sidebar icons

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,12 +1,37 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import bird from '../Assets/bird.png';
+import house from '../Assets/home.png';
+import hashtag from '../Assets/hashtag.png';
+import bell from '../Assets/bell.png';
+import envelope from '../Assets/envelope.png';
 import '../styles/sidebar.css';
+
 
 const Sidebar = () => {
 
+  function onClickHouse() {
+    console.log('home');
+  }
+
+  function onClickHashtag() {
+    console.log('hashtag');
+  }
+
+  function onClickBell() {
+    console.log('bell');
+  }
+
+  function onClickEnvelope() {
+    console.log('envelope');
+  }
+
   return (
     <div>
-     <img src = {bird} id = 'bird' alt = 'blue twitter bird'/>   
+     <img src = {bird} id = 'bird' alt = 'blue twitter bird'/>
+     <img src = {house} id = 'house' alt = 'blue house' onClick = {onClickHouse}/>
+     <img src = {hashtag} id = 'hashtag' alt = 'hashtag' onClick = {onClickHashtag}/>
+     <img src = {bell} id = 'bell' alt = 'bell' onClick = {onClickBell}/>
+     <img src = {envelope} id = 'envelope' alt = 'envelope' onClick = {onClickEnvelope}/>
     </div>
   )
 
