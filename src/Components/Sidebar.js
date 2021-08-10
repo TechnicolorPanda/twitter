2bf1dc96d8cd81ddaadf65b43eb596259b809374
LorriAnn <LorriAnn7@att.net>
2021-08-09 22:09:22 -0400

    added headings to sidebar

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -26,12 +26,12 @@ const Sidebar = () => {
   }
 
   return (
-    <div>
-     <img src = {bird} id = 'bird' alt = 'blue twitter bird'/>
-     <img src = {house} id = 'house' alt = 'blue house' onClick = {onClickHouse}/>
-     <img src = {hashtag} id = 'hashtag' alt = 'hashtag' onClick = {onClickHashtag}/>
-     <img src = {bell} id = 'bell' alt = 'bell' onClick = {onClickBell}/>
-     <img src = {envelope} id = 'envelope' alt = 'envelope' onClick = {onClickEnvelope}/>
+    <div className = 'sidebar'>
+     <div id = 'bird'><img src = {bird} id = 'bird' alt = 'blue twitter bird'/></div>
+     <div id = 'home'><img src = {house} id = 'house' alt = 'blue house' onClick = {onClickHouse}/>Home  </div>
+     <div id = 'explore'><img src = {hashtag} id = 'hashtag' alt = 'hashtag' onClick = {onClickHashtag}/>Explore  </div>
+     <div id = 'notifications'><img src = {bell} id = 'bell' alt = 'bell' onClick = {onClickBell}/>Notifications  </div>
+     <div id = 'messages'><img src = {envelope} id = 'envelope' alt = 'envelope' onClick = {onClickEnvelope}/>Messages  </div>
     </div>
   )
 
